0e4c0c43b44b681afc9d6f6ecbaef7465d13eef9
slavomil <71152550+slavomil@users.noreply.github.com>
2020-09-24 00:26:25 +0300

    Update button.js

--- button.js
+++ button.js
@@ -1,9 +1,6 @@
 let button = document.querySelector('button')
-let number = 0
-button.value = 0 
+button.value = 1 
 button.onclick.function () {
     button.value++    
 }
-number.onclick.function () {
- number.value++
-}
+
